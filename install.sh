e312d1a8beba855a94fd10c000ba087eb28f365c
Trey Lowerison <tlowerison@grailbio.com>
2021-01-09 21:51:38 -0800

    update installations

--- install.sh
+++ install.sh
@@ -1,32 +1,45 @@
 #!/bin/bash
+if [ "$1" = "" ]; then
+  echo "Please provide a project name"
+  exit 1
+fi
+if [ "$2" = "" ]; then
+  echo "Please provide a domain name you own"
+  exit 1
+fi
+
+DIR=$(dirname "$BASH_SOURCE")
+
+cd "$DIR"
+
 rm -rf .git
 
 if [ "$3" != "" ]; then
   grep -rfl 'grandstackkit' k8s/ | xargs sed -i 's/registry.digitalocean.com/grandstack-kit/$3/g'
 fi
 
-mv ../{grandstack-kit,$1}
+grep -rl 'grandstack-kit' api/ | xargs sed -i "" "s/grandstack-kit/$1/g"
+grep -rl 'grandstack-kit' client/ | xargs sed -i "" "s/grandstack-kit/$1/g"
+grep -rl 'grandstack-kit' k8s/ | xargs sed -i "" "s/grandstack-kit/$1/g"
+grep -rl 'grandstack-kit' server/ | xargs sed -i "" "s/grandstack-kit/$1/g"
 
-grep -rl 'grandstack-kit' api/ | xargs sed -i "s/grandstack-kit/$1/g"
-grep -rl 'grandstack-kit' client/ | xargs sed -i "s/grandstack-kit/$1/g"
-grep -rl 'grandstack-kit' k8s/ | xargs sed -i "s/grandstack-kit/$1/g"
-grep -rl 'grandstack-kit' server/ | xargs sed -i "s/grandstack-kit/$1/g"
-
-grep -rl 'domain.com' api/ | xargs sed -i "s/domain.com/$2/g"
-grep -rl 'domain.com' client/ | xargs sed -i "s/domain.com/$2/g"
-grep -rl 'domain.com' k8s/ | xargs sed -i "s/domain.com/$2/g"
-grep -rl 'domain.com' server/ | xargs sed -i "s/domain.com/$2/g"
+grep -rl 'domain.com' api/ | xargs sed -i "" "s/domain.com/$2/g"
+grep -rl 'domain.com' client/ | xargs sed -i "" "s/domain.com/$2/g"
+grep -rl 'domain.com' k8s/ | xargs sed -i "" "s/domain.com/$2/g"
+grep -rl 'domain.com' server/ | xargs sed -i "" "s/domain.com/$2/g"
 
 cd api && yarn && yarn build && cd ..
 cd client && yarn && yarn build && cd ..
 cd server && yarn && yarn build && cd ..
 
 cd server
-wget http://download.redis.io/redis-stable.tar.gz
+wget -O http://download.redis.io/redis-stable.tar.gz
 tar xvzf redis-stable.tar.gz
 cd redis-stable
 make
-cd ../..
+cd ..
+rm redis-stable.tar.gz
+cd ..
 
 cd scripts
 chmod u+x build-image.sh
@@ -36,3 +49,7 @@ chmod u+x yield-image.sh
 cd ..
 
 git init
+cd ..
+
+mv {$DIR,$1}
+cd "$1"
